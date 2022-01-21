519251a3e15bd704dc24010527a7e43698fe3f98
webdeveloper510 <developerweb6@gmail.com>
2022-01-22 03:24:29 +0530

    corrected alert messages on template and made requirements.txt

--- PaypalProj/settings.py
+++ PaypalProj/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
+from django.contrib.messages import constants as messages
 ROOT_PATH = os.path.dirname(__file__)
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -28,6 +29,8 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 
+
+
 try:
     import pymysql
     pymysql.install_as_MySQLdb()
@@ -35,6 +38,14 @@ except:
     pass
 
 # Application definition
+
+MESSAGE_TAGS = {
+        messages.DEBUG: 'alert-secondary',
+        messages.INFO: 'alert-info',
+        messages.SUCCESS: 'alert-success',
+        messages.WARNING: 'alert-warning',
+        messages.ERROR: 'alert-danger',
+ }
 
 INSTALLED_APPS = [
     'django.contrib.admin',
